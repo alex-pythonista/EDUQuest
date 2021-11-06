1db1c4ba454c8aa9dc0ba7356100259a2d83f4f4
alex-pythonista <indrajit.alex@gmail.com>
2021-11-06 14:57:28 +0600

    Login-Register templates merged

--- loginApp/views.py
+++ loginApp/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+def home(request):
+    return HttpResponse('Home page')
+
 
 def login_page(request):
     context = {}
-    return render(request, 'loginApp/login.html', context)
+    template_name = 'loginApp/login.html'
+    return render(request, template_name, context)
+
+def register(request):
+    context = {}
+    template_name = 'loginApp/register.html'
+    return render(request, template_name, context)
