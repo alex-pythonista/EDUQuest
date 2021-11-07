36b7a58189149b5a2c56ada94f4d84d83a838c0b
alex-pythonista <indrajit.alex@gmail.com>
2021-11-07 13:26:37 +0600

    DB updated

--- loginApp/models.py
+++ loginApp/models.py
@@ -7,7 +7,7 @@ class Student(models.Model):
     user = models.OneToOneField(User, on_delete=models.DO_NOTHING, related_name='student_user')
     fullname = models.CharField(max_length=200)
     department = models.CharField(max_length=100)
-    semester = models.CharField(max_length=100)
+    semester = models.CharField(max_length=50)
     student_id = models.CharField(max_length=9)
     address = models.CharField(max_length=500)
 
