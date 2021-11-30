884d7bb8ed1a0457cd0ed77c7abe6cb901eb18e1
alex-pythonista <indrajit.alex@gmail.com>
2021-12-01 01:14:37 +0600

    Bug Fixed

--- loginApp/models.py
+++ loginApp/models.py
@@ -11,9 +11,9 @@ class Student(models.Model):
     student_id = models.CharField(max_length=10, primary_key=True)
     address = models.CharField(max_length=500, default='UNKNOWN')
     cgpa = models.DecimalField(max_digits=3, decimal_places=2, blank=True, null=True)
-    credit_taken = models.DecimalField(max_digits=3, decimal_places=2, blank=True, null=True) 
-    credit_completed = models.DecimalField(max_digits=3, decimal_places=2, blank=True, null=True)
-    #profile_picture = models.ImageField(upload_to='student_profile', blank=True, null=True)
+    credit_taken = models.DecimalField(max_digits=4, decimal_places=2, blank=True, null=True) 
+    credit_completed = models.DecimalField(max_digits=5, decimal_places=2, blank=True, null=True)
+    profile_picture = models.ImageField(upload_to='student_profile', blank=True, null=True)
 
     def __str__(self):
         return self.fullname + " " + self.student_id
--- studentApp/views.py
+++ studentApp/views.py
@@ -12,47 +12,57 @@ def dev_page(request):
     template_name = 'dev.html'
     return render(request, template_name, context)
 
+
+@login_required
 def profile_view(request):
     studentProfile = Student.objects.get(user=request.user)
     context = {'student': studentProfile}
     template_name = 'studentApp/profile.html' 
-    return render(request,template_name, context)
+    return render(request, template_name, context)
 
+@login_required
 def catalog_view(request,*args, **kwargs):
     context = {}
     template_name = 'more_about_courses.html'
     # return HttpResponse("<h1>Course Catalog</h1>") 
     return render(request,template_name, context)
 
+@login_required
 def routine_view(request,*args, **kwargs): 
     context = {}
     template_name = 'routineApp/routine.html'
     # return HttpResponse("<h1>Personalized Routine</h1>") 
     return render(request,template_name, context)
 
+@login_required
 def schedule_checker_view(request,*args, **kwargs): 
     context = {}
     template_name = 'schedule_checker.html'
     return render(request,template_name, context)
 
+@login_required
 def progress_view(request,*args, **kwargs): 
     context = {}
     template_name = 'progress.html'
     # return HttpResponse("<h1>Course Progress</h1>") 
     return render(request,template_name, context)
 
+@login_required
 def instructor_view(request,*args, **kwargs): 
     context = {}
     template_name = 'instructor.html'
     #return HttpResponse("<h1>Instructor Profile</h1>") 
     return render(request,template_name, context)
 
+@login_required
 def finances_view(request,*args, **kwargs): 
     context = {}
     template_name = 'finances.html'
     #return HttpResponse("<h1>Finances</h1>")
     return render(request,template_name, context)
         
+        
+@login_required       
 def bus_schedule_view(request,*args, **kwargs): 
     context = {}
     template_name = 'bus_schedule.html'
